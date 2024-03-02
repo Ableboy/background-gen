f183a1ff2c242d944a6e1033b4964fe9e1084f89
Ableboy <ebubemoses235@gmail.com>
2024-03-02 23:15:03 +0100

    remove hash sign and little code changes

--- pi.py
+++ pi.py
@@ -1,25 +1,23 @@
-# def show_tree(): # use function to draw a tree
-#   picture = [
-#     [0,0,0,1,0,0,0],
-#     [0,0,1,1,1,0,0],
-#     [0,1,1,1,1,1,0],
-#     [1,1,1,1,1,1,1],
-#     [0,0,0,1,0,0,0],
-#     [0,0,0,1,0,0,0],
-#   ]
+def show_tree(): # use function to draw a tree
+  picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0],
+  ]
     
-#   space = 0 # var access the 0 in nested list
-#   # var access the 1 in nested list
-#   aesteric = 1
-#   for row in picture: # row in the nested list
-#     for item in row: # item takes the row value
-#       # print out the space and asteric
-#       if item == space: 
-#         print(" ", end = "")
-#       elif item == aesteric:
-#         print("*", end = "")
-#     print(" ")
-#   print("\n")# need a space after each row
-# show_tree()
-# show_tree()
-# print(show_tree)
+  space = 0 # var access the 0 in nested list
+  # var access the 1 in nested list
+  aesteric = 1
+  for row in picture: # row in the nested list
+    for item in row: # item takes the row value
+      # print out the space and asteric
+      if item == space: 
+        print(" ", end = "")
+      elif item == aesteric:
+        print("*", end = "")
+    print(" ")
+  print("\n")# need a space after each row
+show_tree()
