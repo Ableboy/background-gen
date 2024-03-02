9e5e7e74b1391917c3bba8e7e9c30a0050cfc4ba
Ebube Nwoke <117966877+Ableboy@users.noreply.github.com>
2024-03-02 22:57:01 +0100

    Update pi.py

--- pi.py
+++ pi.py
@@ -10,16 +10,14 @@ def show_tree(): # use function to draw a tree
     
   space = 0 # var access the 0 in nested list
   # var access the 1 in nested list
-  aesteric = 1
+  aesthetic = 1
   for row in picture: # row in the nested list
     for item in row: # item takes the row value
-      # print out the space and asteric
+      # Print out the space and aesthetic
       if item == space: 
         print(" ", end = "")
-      elif item == aesteric:
+      elif item == aesthetic:
         print("*", end = "")
     print(" ")
   print("\n")# need a space after each row
 show_tree()
-show_tree()
-print(show_tree)
